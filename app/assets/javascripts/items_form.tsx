654ac9db5d9ba751215a61d38f3fbcb2894fdc79
benji <benj_cous@hotmail.com>
2017-04-07 14:56:04 +0200

    Add webpack and react

---
+++ app/assets/javascripts/items_form.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+class ItemsForm extends React.Component<{}, {}> {
+  render() {
+    return (
+      <div>hallo</div>
+    );
+  }
+}
+
+render(<ItemsForm />, document.getElementById('temp'));
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var webpack           = require('webpack');
+var path              = require('path');
+
+var APP_DIR = path.resolve(__dirname, 'app', 'assets', 'javascripts');
+var config = {
+  entry: {
+    javascript: APP_DIR + '/items_form.tsx'
+  },
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts', 'bundles'),
+    filename: 'items_form.js'
+  },
+  module: {
+    loaders : [
+      {
+        test: /\.tsx?$/,
+        include: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+        use: [ { loader : 'babel-loader' }, { loader: 'ts-loader'}]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      React: 'react'
+    })
+  ]
+};
+
+module.exports = config;
